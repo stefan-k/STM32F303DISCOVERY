4143e8bdee80d95b92d33f617e10d03a62952f7f
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-02-10 22:56:47 +0100

    registers

--- discovery/src/07-registers/src/main.rs
+++ discovery/src/07-registers/src/main.rs
@@ -8,51 +8,61 @@ use core::ptr;
 #[inline(never)]
 #[no_mangle]
 pub fn main() -> ! {
-    unsafe {
-        // A magic address!
-        const GPIOE_BSRR: u32 = 0x48001018;
-        const GPIOE_ODR: u32 = 0x4800_1014;
+    use pg::peripheral;
 
-        iprintln!(
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *const u16)
-        );
+    let gpioe = unsafe { peripheral::gpioe_mut() };
 
-        // Turn on the "North" LED (red)
-        // *(GPIOE_BSRR as *mut u32) = 1 << 9;
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
-        iprintln!(
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *const u16)
-        );
-
-        // Turn on the "East" LED (green)
-        // *(GPIOE_BSRR as *mut u32) = 1 << 11;
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
-        iprintln!(
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *const u16)
-        );
-
-        // Turn off the "North" LED
-        // *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
-        iprintln!(
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *const u16)
-        );
-
-        // Turn off the "East" LED
-        // *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
-        ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
-        iprintln!(
-            "ODR = 0x{:04x}",
-            ptr::read_volatile(GPIOE_ODR as *const u16)
-        );
-
-        // 0xBAAAAAAD address
-        // ptr::read_volatile(0x4800_1800 as *const u32);
-    }
-
+    gpioe.bsrr.write(|w| w.bs9(true));
+    gpioe.bsrr.write(|w| w.bs11(true));
+    gpioe.bsrr.write(|w| w.br9(true));
+    gpioe.bsrr.write(|w| w.br11(true));
     loop {}
+
+    // unsafe {
+    //     // A magic address!
+    //     const GPIOE_BSRR: u32 = 0x48001018;
+    //     const GPIOE_ODR: u32 = 0x4800_1014;
+    //
+    //     iprintln!(
+    //         "ODR = 0x{:04x}",
+    //         ptr::read_volatile(GPIOE_ODR as *const u16)
+    //     );
+    //
+    //     // Turn on the "North" LED (red)
+    //     // *(GPIOE_BSRR as *mut u32) = 1 << 9;
+    //     ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 9);
+    //     iprintln!(
+    //         "ODR = 0x{:04x}",
+    //         ptr::read_volatile(GPIOE_ODR as *const u16)
+    //     );
+    //
+    //     // Turn on the "East" LED (green)
+    //     // *(GPIOE_BSRR as *mut u32) = 1 << 11;
+    //     ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << 11);
+    //     iprintln!(
+    //         "ODR = 0x{:04x}",
+    //         ptr::read_volatile(GPIOE_ODR as *const u16)
+    //     );
+    //
+    //     // Turn off the "North" LED
+    //     // *(GPIOE_BSRR as *mut u32) = 1 << (9 + 16);
+    //     ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (9 + 16));
+    //     iprintln!(
+    //         "ODR = 0x{:04x}",
+    //         ptr::read_volatile(GPIOE_ODR as *const u16)
+    //     );
+    //
+    //     // Turn off the "East" LED
+    //     // *(GPIOE_BSRR as *mut u32) = 1 << (11 + 16);
+    //     ptr::write_volatile(GPIOE_BSRR as *mut u32, 1 << (11 + 16));
+    //     iprintln!(
+    //         "ODR = 0x{:04x}",
+    //         ptr::read_volatile(GPIOE_ODR as *const u16)
+    //     );
+    //
+    //     // 0xBAAAAAAD address
+    //     // ptr::read_volatile(0x4800_1800 as *const u32);
+    // }
+    //
+    // loop {}
 }
